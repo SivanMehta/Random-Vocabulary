f502aedf8f20f519596a4a6a18bed92eb70f9e6f
Sivan Mehta <skmehta@andrew.cmu.edu>
2015-11-17 20:47:51 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,31 @@
+var morgan = require('morgan');
+var fs = require('fs');
+var express = require('express');
+var app = express();
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(morgan('tiny'));
+
+random_page = function(request, response)
+{
+    if(Math.random() > .5)
+    {
+        response.render("picture", {});
+    }
+    else
+    {
+        response.render("words")
+    }
+}
+
+app.get("*", random_page);
+app.use(random_page);
+
+ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+port = process.env.OPENSHIFT_NODEJS_PORT || 50000;
+
+//  Start listening on the specific IP and PORT
+app.listen(port, ipaddress, function() {
+  console.log('%s: Server started on %s:%d ...', Date(Date.now()), ipaddress, port);
+});
